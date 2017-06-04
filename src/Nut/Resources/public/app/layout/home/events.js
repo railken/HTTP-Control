11c68d07e060a4623fd4da09565859cd554f447a
railken <railken@protonmail.ch>
2017-06-04 21:39:09 +0200

    added BasicResource

--- src/Nut/Resources/public/app/layout/home/events.js
+++ src/Nut/Resources/public/app/layout/home/events.js
@@ -24,125 +24,103 @@ var ButtonAjaxLoader = function(button)
 	};
 };
 
+var BasicResource = function(resolver)
+{
+	this.resolver = resolver;
+
+	this.create = function(form)
+	{
+
+		var params = getParamsByForm(form);
+
+		var resolver = this.resolver;
+
+		resolver.create({
+			attributes: params,
+			success: function()
+			{
+				form.find("[name]").val('');
+			}
+		});
+	};
+
+	this.update = function(form)
+	{
+
+		var params = getParamsByForm(form);
+
+		var resolver = this.resolver;
+
+		var next = function() { 
+			resolver.update(params.id, {
+				attributes: params,
+				success: function()
+				{
+					form.find("[name]").val('');
+					btn_loader.end();
+				}
+			});
+		}
+
+		var btn_loader = new ButtonAjaxLoader($(this).find("[type='submit']"));
+		btn_loader.start();
+
+		if (typeof params.avatar !== "undefined") {
+
+			File.onLoad('avatar', function(value) {
+				params.avatar = value;
+				next()
+			});
+		}
+	}
+
+	this.delete = function(form)
+	{
+		$('.modal').modal('hide');
+		var id = form.find("[name='id']").val();
+
+		var resolver = this.resolver;
+
+		setTimeout(function() {
+			resolver.remove(id, {});
+		}, 100);
+	}
+};
 
 $('body').on('submit', "[name='projects.create']", function(e) {
 	e.preventDefault();
+	br = new BasicResource(new ProjectResolver());
+	br.create($(this));
+});
 
-	var params = getParamsByForm($(this));
-
-	var resolver = new ProjectResolver();
-
-	resolver.create({
-		attributes: {	
-			name: params.name,
-			description: params.description,
-			team_id: params.team_id
-		},
-		success: function()
-		{
-			$(this).find("[name]").val('');
-		}
-	});
+$('body').on('submit', "[name='projects.update']", function(e) {
+	e.preventDefault();
+	br = new BasicResource(new ProjectResolver());
+	br.update($(this));
 });
 
 $('body').on('submit', "[name='projects.remove']", function(e) {
 	e.preventDefault();
-	$('.modal').modal('hide');
-	var id = $(this).find("[name='id']").val();
-
-	var resolver = new ProjectResolver();
-
-	setTimeout(function() {
-		resolver.remove(id, {});
-	}, 100);
-
+	br = new BasicResource(new ProjectResolver());
+	br.remove($(this));
 });
-
-
-$('body').on('submit', "[name='projects.update']", function(e) {
-	
-	e.preventDefault();
-
-	var params = getParamsByForm($(this));
-
-	var resolver = new ProjectResolver();
-
-
-	resolver.update(params.id, {
-		attributes: {	
-			name: params.name,
-			description: params.description
-		},
-		success: function()
-		{
-			$(this).find("[name]").val('');
-		}
-	});
-
-});
-
 
 $('body').on('submit', "[name='teams.create']", function(e) {
 	e.preventDefault();
-
-	var params = getParamsByForm($(this));
-
-	var resolver = new TeamResolver();
-
-	resolver.create({
-		attributes: {	
-			name: params.name,
-			description: params.description
-		},
-		success: function()
-		{
-			$(this).find("[name]").val('');
-		}
-	});
-
+	br = new BasicResource(new TeamResolver());
+	br.create($(this));
 });
 
 $('body').on('submit', "[name='teams.update']", function(e) {
 	e.preventDefault();
-
-	var params = getParamsByForm($(this));
-
-	var resolver = new TeamResolver();
-
-	var next = function() { 
-		resolver.update(params.id, {
-			attributes: params,
-			success: function()
-			{
-				$(this).find("[name]").val('');
-				btn_loader.end();
-			}
-		});
-	}
-
-	var btn_loader = new ButtonAjaxLoader($(this).find("[type='submit']"));
-	btn_loader.start();
-
-	if (typeof params.avatar !== "undefined") {
-
-		File.onLoad('avatar', function(value) {
-			params.avatar = value;
-			next()
-		});
-	}
-
+	br = new BasicResource(new TeamResolver());
+	br.update($(this));
 });
 
 $('body').on('submit', "[name='teams.remove']", function(e) {
 	e.preventDefault();
-	$('.modal').modal('hide');
-	var params = getParamsByForm($(this));
-	var resolver = new TeamResolver();
-
-	setTimeout(function() {
-		resolver.remove(params.id, {});
-	}, 100);
-	
+	br = new BasicResource(new TeamResolver());
+	br.remove($(this));
 });
 
 $('body').on('show.bs.modal', "[data-modal-type='team']", function (event) {
